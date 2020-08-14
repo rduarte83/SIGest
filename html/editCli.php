150820b2f80f19e69abec2f153fafdc196f9ab0b
Rui Duarte <r3v3nant@gmail.com>
2020-08-14 17:02:23 +0100

    editCli

--- html/editCli.php
+++ html/editCli.php
@@ -99,6 +99,13 @@
                                     <input type="email" id="email" name="email" class="form-control">
                                 </div>
                             </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Comercial</label>
+                                <div class="col-sm-10">
+                                    <select name="comercial" id="comercial" class="form-control">
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                         <!-- /.card-body -->
                         <div class="card-footer">
